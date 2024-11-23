b5dc3193ceb07b60337cb1d072f570c555d01ee9
Rakib48-code <shahidulhuda0@gmail.com>
2024-11-23 12:28:55 +0600

    inheritance done

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Open Academy",
     'sequence': '-100',
     'version': '1.0',
-    'depends': ['base', 'web'],
+    'depends': ['base'],
     'author': "Rakib Hasan",
     'category': 'Education',
     'description': """
@@ -15,6 +15,7 @@
         'views/open_academy_menu.xml',
         'views/course_view_menu.xml',
         'views/session_view.xml',
+        'views/partner.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -11,3 +11,21 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string='Instructor')
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string='Course', required=True)
     attendance_id = fields.Many2many('res.partner', string='Attendace')
+    # taken_seats = fields.Float(string='Taken seats', compute='_taken_seats')
+    # date_end = fields.Date(string='End Date', store=True, compute='_get_end_date', inverse='_set_end_date')
+    # attendace_count = fields.Integer(string='Attendacne Count', compute='_get_attendance_count', store=True)
+    # color = fields.Integer()
+
+
+    # @api.depends('seats','attendance_id')
+    # def _taken_seats(self):
+    #     for r in self:
+    #         if not r.seats:
+    #             r.taken_seats = 0.0
+    #         else:
+    #             r.taken_seats = 100.0 * len(r.attendance_id) / r.seats
+    #
+    # @api.depends('attendance_id')
+    # def _get_attendance_count(self):
+    #     for r in self:
+    #         r.attendees_count = len(r.attendee_ids)
